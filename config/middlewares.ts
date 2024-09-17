8d3452d70cbc7e71d61eaf66ee0ca97eb53c0ff4
d.zajenckauskas <d.zajenckauskas@gmail.com>
2024-09-17 08:49:09 +0000

    updates

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,17 @@ export default [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb', // Set form data limit
+      jsonLimit: '256mb', // Set JSON data limit
+      textLimit: '256mb', // Set text data limit
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // Set max upload file size to 100MB
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
